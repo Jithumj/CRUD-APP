9b978de17b2a61fb9392631d30110a94799ece51
Jithumj <Dev.j2910@gmail.com>
2024-05-26 01:49:11 +0530

    crude app done

---
+++ Backend-Api/CrudAppReact/Controllers/CrudController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CrudAppReact.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CrudController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        public CrudController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        [Route("GetNotes")]
+        public JsonResult GetNotes()
+        {
+            string query = "select * from dbo.notes";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("CrudAppDBCon");
+            SqlDataReader myReader;
+            using (SqlConnection mycon = new SqlConnection(sqlDatasource))
+            {
+                mycon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, mycon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        [Route("AddNotes")]
+        public JsonResult AddNotes([FromForm] string newNotes)
+        {
+            string query = "insert into dbo.notes values(@newNotes)";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("CrudAppDBCon");
+            SqlDataReader myReader;
+            using (SqlConnection mycon = new SqlConnection(sqlDatasource))
+            {
+                mycon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@newNotes", newNotes);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+            return new JsonResult("Added Succesfully");
+        }
+
+        [HttpDelete]
+        [Route("DeleteNotes")]
+        public JsonResult DeleteNotes(int id)
+        {
+            string query = "delete from dbo.notes where id=@id";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("CrudAppDBCon");
+            SqlDataReader myReader;
+            using (SqlConnection mycon = new SqlConnection(sqlDatasource))
+            {
+                mycon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@id", id);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+            return new JsonResult("Deleted Succesfully");
+        }
+        [HttpPut]
+        [Route("UpdateNotes")]
+        public JsonResult UpdateNotes(string description,int id)
+        {
+            string query = "update dbo.notes set description=@description where id=@id";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("CrudAppDBCon");
+            SqlDataReader myReader;
+            using (SqlConnection mycon = new SqlConnection(sqlDatasource))
+            {
+                mycon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@id", id);
+                    myCommand.Parameters.AddWithValue("@description", description);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+            return new JsonResult("Updated Succesfully");
+        }
+    }
+}
---
+++ Backend-Api/CrudAppReact/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Json serializer
+builder.Services.AddControllers().AddNewtonsoftJson(Options=>Options.
+SerializerSettings.ReferenceLoopHandling=Newtonsoft.Json.ReferenceLoopHandling.Ignore).AddNewtonsoftJson(
+    options=>options.SerializerSettings.ContractResolver=new DefaultContractResolver());
+
+var app = builder.Build();
+
+//Enable CORS
+app.UseCors(c => c.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
